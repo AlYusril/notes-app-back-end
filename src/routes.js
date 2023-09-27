f79830d3c389b677faa9e5d5e1860a1ecbe43285
fakehthok <fakehpancen99@gmial.com>
2023-09-27 21:56:30 +0700

    F_route.js_edit

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,10 @@
+const{addNoteHandler} = require('./handler');
 const routes =[
     {
         method: 'POST',
         path:'/notes',
-        handler:()=>{},
+        handler: addNoteHandler,
     },
-    // {
-    //     method:'*',
-    //     path:'/',
-    //     handler:(request, h)=>{
-    //         return 'Halaman tidak dapat diakses dengan method tersebut';
-    //     },
-    // },
-    // {
-    //     method:'GET',
-    //     path
-    // }
 
 ];
 module.exports = routes;
