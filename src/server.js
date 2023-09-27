a0ec38f028a68209079da8a64565d0e5d7693fdf
AlYusril <yusrilefendi12345@gmail.com>
2023-09-27 21:38:40 +0700

    Y_server.js&handler.js

--- src/server.js
+++ src/server.js
@@ -1,1 +1,10 @@
-console.log('Hallo kita akan membuat RESTful API');
+const Hapi=require('@hapi/hapi');
+const init = async () => {
+    const server = Hapi.server({
+        port: 5100,
+        host:'localhost',
+    });
+    await server.start();
+    console.log('Server berjalan pada ${server.info.uri}');
+};
+init();
