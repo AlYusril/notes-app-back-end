9d16187307e140afc33666dffbd6dda3b99022fa
fakehthok <fakehpancen99@gmial.com>
2023-09-27 21:37:58 +0700

    f_route&notes

---
+++ src/routes.js
@@ -0,0 +1,20 @@
+const routes =[
+    {
+        method: 'POST',
+        path:'/notes',
+        handler:()=>{},
+    },
+    // {
+    //     method:'*',
+    //     path:'/',
+    //     handler:(request, h)=>{
+    //         return 'Halaman tidak dapat diakses dengan method tersebut';
+    //     },
+    // },
+    // {
+    //     method:'GET',
+    //     path
+    // }
+
+];
+module.exports = routes;
