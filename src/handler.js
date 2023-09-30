07c76f1e28fdc81d652914abf76ad9c065b8718d
sgtadwiii <sagtadwi123@gmail.com>
2023-09-30 09:41:21 +0700

    Sg-Add-Handler-GetNoteHandlerr

--- src/handler.js
+++ src/handler.js
@@ -46,6 +46,28 @@ const getAllNotesHandler = () => ({
   },
 });
 
+const getNoteByIdHandler = (request, h) => {
+  const { id } = request.params;
+
+  const note = notes.filter((n) => n.id === id)[0];
+
+  if (note !== undefined) {
+    return {
+      status: "success",
+      data: {
+        note,
+      },
+    };
+  }
+
+  const response = h.response({
+    status: "fail",
+    message: "Catatan tidak ditemukan",
+  });
+  response.code(404);
+  return response;
+};
+
 const editNoteByIdHandler = (request, h) => {
   const { id } = request.params;
 
@@ -81,5 +103,6 @@ const editNoteByIdHandler = (request, h) => {
 module.exports = {
   addNoteHandler,
   getAllNotesHandler,
+  getNoteByIdHandler,
   editNoteByIdHandler,
 };
